72a10d57805c1eb06704777157043c18a8a813ab
Mark Frederick <mark@makusafe.com>
2017-11-10 14:37:50 -0600

    working on capturing input data into the gcf

--- index.js
+++ index.js
@@ -4,14 +4,33 @@
  * @param {!Object} event The Cloud Functions event.
  * @param {!Function} The callback function.
  */
+const https = require('https');
+
 exports.subscribe = function subscribe(event, callback) {
     // The Cloud Pub/Sub Message object.
     const pubsubMessage = event.data;
-  
-    // We're just going to log the message to prove that
-    // it worked.
-    console.log(Buffer.from(pubsubMessage.data, 'base64').toString());
-  
+
+    Console.log(pubsubMessage);
+
+    /*
+    https.get('https://api.makusafe.com/v1/incidentLocation', (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;f
+        });
+       
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          console.log(JSON.parse(data).explanation);
+        });
+       
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
+    */
+
     // Don't forget to call the callback.
     callback();
   };
